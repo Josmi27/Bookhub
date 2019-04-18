7e0b0f90c038eb44fd2d2e6fdcf215e899a6c6cc
Joshua Smith <30632852+Josmi27@users.noreply.github.com>
2019-04-18 11:53:54 -0700

    Minor Fixes to Explore Page

--- app/config.py
+++ app/config.py
@@ -7,4 +7,4 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    POSTS_PER_PAGE = 25
+    RECOMMENDATIONS_PER_PAGE = 1
--- app/routes.py
+++ app/routes.py
@@ -10,16 +10,22 @@ from datetime import datetime
 @app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-       # recommendation = Recommendation(book_title)
-        recommender=current_user
+        form = RecommendationForm()
+        if form.validate_on_submit():
+                recommendation = Recommendation(book_title=form.book_title.data, book_author=form.book_author.data, book_category=form.book_category.data, book_summary=form.book_summary.data, author=current_user)
+                db.session.add(recommendation)
+                db.session.commit()
+                flash('Awesome, your recommendation is now posted!')
+                return redirect(url_for('index'))
+
         page = request.args.get('page', 1, type=int)
         recommendations = current_user.followed_posts().paginate(
-                page, app.config['POSTS_PER_PAGE'], False)
+                page, app.config['RECOMMENDATIONS_PER_PAGE'], False)
         next_url = url_for('index', page=recommendations.next_num) \
                 if recommendations.has_next else None
         prev_url = url_for('index', page = recommendations.prev_num) \
                 if recommendations.has_prev else None
-        return render_template('index.html', title='Home', recommender=recommender, recommendations=recommendations.items, next_url=next_url, prev_url=prev_url)
+        return render_template('index.html', title='Home', recommendations=recommendations.items, next_url=next_url, prev_url=prev_url)
 
 
 # Function for routing recommendation button
@@ -45,25 +51,17 @@ def new_recommendation():
 @login_required
 def explore():
 
-        recommender=current_user
         page = request.args.get('page', 1, type=int)
-        recommendations = current_user.followed_posts().paginate(
-                page, app.config['POSTS_PER_PAGE'], False)
-        return render_template('index.html', title='Home', recommender=recommender, recommendations=recommendations.items)
-
-
-#     page = request.args.get('page', 1, type=int)
-#     recommendations = Recommendation.query.order_by(Recommendation.timestamp.desc()).paginate(
-#         page, app.config['POSTS_PER_PAGE'], False)
-#     next_url = url_for('explore', page=recommendations.next_num) \
-#         if recommendations.has_next else None
-#     prev_url = url_for('explore', page=recommendations.prev_num) \
-#         if recommendations.has_prev else None
-#     return render_template("index.html", title='Explore', author=current_user, recommendations=recommendations.items, next_url=next_url, prev_url=prev_url)
-#     page = request.args.get('page', 1, type=int)
-#     recommendations = Recommendation.query.order_by(Recommendation.timestamp.desc()).all()
-# #     recommender=current_user
-#     return render_template('index.html', title='Explore', recommendations=recommendations)
+        recommendations = Recommendation.query.order_by(Recommendation.timestamp.desc()).paginate(
+        page, app.config['RECOMMENDATIONS_PER_PAGE'], False)
+        next_url = url_for('explore', page=recommendations.next_num) \
+                if recommendations.has_next else None
+        prev_url = url_for('explore', page=recommendations.prev_num) \
+                if recommendations.has_prev else None
+        return render_template("index.html", title='Explore', recommendations=recommendations.items, next_url=next_url, prev_url=prev_url)
+        page = request.args.get('page', 1, type=int)
+        recommendations = Recommendation.query.order_by(Recommendation.timestamp.desc()).all()
+        return render_template('index.html', title='Explore', recommendations=recommendations)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -111,7 +109,7 @@ def user(username):
         user = User.query.filter_by(username=username).first_or_404()
         page = request.args.get('page', 1, type=int)
         recommendations = user.recommendations.order_by(Recommendation.timestamp.desc()).paginate(
-                page, app.config['POSTS_PER_PAGE'], False)
+                page, app.config['RECOMMENDATIONS_PER_PAGE'], False)
         next_url = url_for('user', username=user.username, page=recommendations.next_num) \
                 if recommendations.has_next else None
         prev_url = url_for('user', username=user.username, page=recommendations.prev_num) \
